4b2ac81346cffbd875745c8b767be062e2887a20
Martin Riedel <riedelm@users.sourceforge.net>
2010-05-14 13:15:30 +0200

    removed unnecessary import

--- src/net/sourceforge/transfile/ui/swing/SwingGUI.java
+++ src/net/sourceforge/transfile/ui/swing/SwingGUI.java
@@ -26,7 +26,6 @@ import static net.sourceforge.transfile.ui.swing.StatusService.StatusMessage;
 
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
