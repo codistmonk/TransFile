ef5eac5e9dca4a7a180372d0b7551033a839cbdb
codistmonk <codistmonk@users.sourceforge.net>
2010-06-08 02:01:37 +0200

    Added AbstractDataMessage.

---
+++ src/net/sourceforge/transfile/operations/AbstractDataMessage.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright © 2010 Martin Riedel
+ * 
+ * This file is part of TransFile.
+ *
+ * TransFile is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TransFile is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TransFile.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.sourceforge.transfile.operations;
+
+import java.io.File;
+
+/**
+ * TODO doc
+ *
+ * @author codistmonk (creation 2010-06-08)
+ *
+ */
+@SuppressWarnings("serial")
+public abstract class AbstractDataMessage extends AbstractOperationMessage implements DataMessage {
+	
+	private final long firstByteOffset;
+	
+	/**
+	 * @param sourceFile
+	 * <br>Should not be null
+	 * <br>Shared parameter
+	 * @param firstByteOffset
+	 * <br>Range: {@code [0L .. Long.MAX_VALUE]}
+	 */
+	public AbstractDataMessage(final File sourceFile, final long firstByteOffset) {
+		super(sourceFile);
+		this.firstByteOffset = firstByteOffset;
+	}
+	
+	@Override
+	public final long getFirstByteOffset() {
+		return this.firstByteOffset;
+	}
+	
+}
